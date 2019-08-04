23f6ceb8fc758f356f3bb6920f583c8e259c4f44
leaty <dev@leaty.net>
2019-08-04 21:38:08 +0200

    Fix steam.sh to use STEAM_RUNTIME_HEAVY and -silent

--- .scripts/steam/steam.sh
+++ .scripts/steam/steam.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-export LD_PRELOAD="/usr/lib/libgio-2.0.so.0 /usr/lib/libglib-2.0.so.0"
-/usr/bin/steam-native
+#export LD_PRELOAD="/usr/lib/libgio-2.0.so.0 /usr/lib/libglib-2.0.so.0"
+export STEAM_RUNTIME_HEAVY=1
+/usr/bin/steam-native -silent
