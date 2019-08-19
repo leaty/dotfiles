a0c8c4ab781bb1ca034de28d984b4e515adaa343
leaty <dev@leaty.net>
2019-08-19 15:17:20 +0200

    Minor adjustments

--- .scripts/background/vidbg.sh
+++ .scripts/background/vidbg.sh
@@ -16,11 +16,24 @@ sleep 1
 #vid='Emerald-Pool-Live-Wallpaper-HD.mp4'
 #vid='Bowl-and-Pitcher-Trail-Stream-Live-Wallpaper.m4v'
 #vid='DesktopHut.com-Florest-Series-Live-Wallpaper-Free.mp4'
-vid='4K-UHD-Flying-Above-Clouds-Live-Wallpaper.mp4'
 #vid='wolf_final.mp4'
 #vid='anime-girl-in-water.mp4'
 #vid='anime-girl-boat-bird.mp4'
 #vid='anime-girl-clown.mp4'
+#vid='4K-UHD-Flying-Above-Clouds-Live-Wallpaper.mp4'
+
+#vid='Rainy-Evening-Anime-Girl-Live-Wallpaper.mp4'
+vid='Grace-Lamp-Anime-Live-Wallpaper.mp4'
+#vid='Girl-With-Flute-In-Rain-Anime-Live-Wallpaper-Free.mp4'
+#vid='Most-Beautiful-World-Hd-Live-Wallpaper.mp4'
+#vid='Rainfall-Girl-Anime-Live-Wallpaper-Free.mp4'
+#vid='Shoto-Todoroki-Boku-No-Hero-Academia-Anime-Live-Wallpaper.mp4'
+#vid='Berserk-The-God-Hand-Live-Wallpaper.mp4'
+#vid='Honkai-Impact-3-Live-Wallpaper-Free.mp4'
+#vid='Touhou-Project-Anime-Live-Wallpaper.mp4'
+#vid='Touhou-Onozuka-Komachi-Live-Wallpaper-Free.mp4'
+#vid='Touhou-Fox-Girl-Anime-Live-Wallpaper-HD.m4v'
+
 
 #xwinwrap -g 1920x1080+1920+0 -ov -ni -fs -s -st -sp -b -nf -- mpv --mute=yes --loop --wid WID ~/Wallpapers/vid/$vid
 xwinwrap -g 1920x1080 -ov -ni -s -nf -- mpv --mute=yes --loop --wid WID ~/Wallpapers/vid/$vid &
