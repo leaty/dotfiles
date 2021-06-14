dba7fcde9f2216eac34d6b26c144fe73a0448ab4
leaty <dev@leaty.net>
2021-06-14 16:33:40 +0200

    Speedup polybar launch time slightly

--- .scripts/polybar/polybar.sh
+++ .scripts/polybar/polybar.sh
@@ -3,8 +3,17 @@
 # Terminate already running bar instances
 killall -q polybar
 
-# Wait a bit
-sleep 0.2
+# Wait a maximum of ~5 seconds for polybar to close
+# Might become higher depending on pgrep speed
+max=$(bc <<< 5/0.01)
+while pgrep -x polybar > /dev/null; do
+  sleep 0.01
+  let count++
+  if [ "$count" -ge "$max" ]; then
+    echo "Reached max; terminating."
+    exit 1
+  fi
+done
 
 # Launch polybar
 MONITOR=$MONITOR1 polybar top &
