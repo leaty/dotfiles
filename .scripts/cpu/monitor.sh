17f6d84a2009f40a3ca77989d52d88cd13a6590b
leaty <dev@leaty.net>
2021-08-31 10:14:02 +0200

    Change cpu monitor script to use corefreq

--- .scripts/cpu/monitor.sh
+++ .scripts/cpu/monitor.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-~/.scripts/urxvt/exec watch -n1 sudo cpupower monitor &
-watch -n0.5 'sensors | grep -e Tc -e Td'
+# Start corefreqd if not running
+if ! systemctl --quiet is-active corefreqd; then
+	sudo modprobe corefreqk
+	sudo systemctl start corefreqd
+fi
+
+corefreq-cli -OE 2 -t
+
+# watch -n0.5 'sensors | grep -e Tc -e Td'
