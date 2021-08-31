4255a42d18816013851e14c379b12770be9c1ca0
leaty <dev@leaty.net>
2021-08-31 20:03:32 +0200

    Add black screen autofixer for LoL

---
+++ .scripts/games/lol.monitor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+client=leagueclientux.exe
+after=',"old":{'
+
+# Monitors switching to any workspace which holds the league client
+# Then runs lol.redraw.sh
+i3-msg -t subscribe -m '[ "workspace" ]' \
+	| while read -r event; do \
+		if echo $event | grep -o "$client.*" | grep -q "$after"; then \
+			~/.scripts/games/lol.redraw.sh; \
+		fi \
+	done
